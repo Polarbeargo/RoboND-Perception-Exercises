c81e69881fbd84b6a42e6d030c8a2c627377bd03
polarbeargo <bow1226@gmail.com>
2018-06-05 23:00:09 -0700

    voxgrid downsample,pass through filter, plane fitting and extract inliers and outliers

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -11,7 +11,7 @@ vox = cloud.make_voxel_grid_filter()
 # Choose a voxel (also known as leaf) size
 # Note: this (1) is a poor choice of leaf size   
 # Experiment and find the appropriate size!
-LEAF_SIZE = 1   
+LEAF_SIZE = 0.01   
 
 # Set the voxel (or leaf) size  
 vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
@@ -28,8 +28,8 @@ passthrough = cloud_filtered.make_passthrough_filter()
 # Assign axis and range to the passthrough filter object.
 filter_axis = 'z'
 passthrough.set_filter_field_name(filter_axis)
-axis_min = 0
-axis_max = 2
+axis_min = 0.6
+axis_max = 1.1
 passthrough.set_filter_limits(axis_min, axis_max)
 
 # Finally use the filter function to obtain the resultant point cloud. 
@@ -48,24 +48,23 @@ seg.set_method_type(pcl.SAC_RANSAC)
 # Max distance for a point to be considered fitting the model
 # Experiment with different values for max_distance 
 # for segmenting the table
-max_distance = 1
+max_distance = 0.01
 seg.set_distance_threshold(max_distance)
 
 # Call the segment function to obtain set of inlier indices and model coefficients
+
 inliers, coefficients = seg.segment()
 
 # Extract inliers
-extracted_inliers = cloud_filtered.extract(inliers, negative=False)
-filename = 'extracted_inliers.pcd'
-pcl.save(extracted_inliers, filename)
+#extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+#filename = 'extracted_inliers.pcd'
+#pcl.save(extracted_inliers, filename)
 
-# Save pcd for table
-pcl.save(cloud, filename)
-
-
-# Extract outliers
+# Extract outlier 
 extracted_outliers = cloud_filtered.extract(inliers, negative=True)
 filename = 'extracted_outliers.pcd'
 pcl.save(extracted_outliers, filename)
+
+
 
 
